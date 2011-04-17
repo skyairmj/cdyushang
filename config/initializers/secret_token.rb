ca955f9f832a7028702c87acc0d917108064387d
Ming Jin <skyairmj@gmail.com>
2011-04-17 17:08:10 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdyushang::Application.config.secret_token = 'b78f0635768b2a14c516cdd42b7b62b07d8cae951f6bc000da4e815d1b1e8aee177dad4b4f23978d98e409af216942e8956adcb14f0b936a57b0fe1ef3d17764'
